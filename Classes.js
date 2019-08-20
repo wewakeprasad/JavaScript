b33b8b0239f695396156f34a56a882655b389ed7
wewakeprasad <wewakeprasad@gmail.com>
2019-08-20 15:52:31 +0530

    Create Classes.js

---
+++ Classes.js
@@ -0,0 +1,21 @@
+//problem Classes on HackerRank
+
+class Polygon
+{
+    constructor(sides)
+    {
+        this.sides = sides;
+
+    }
+
+    perimeter()
+    {
+        var sum = 0;
+        for (var i = 0; i <this. sides.length; i++) {
+            sum = sum + this.sides[i];
+        }
+        return sum;
+    }
+
+}
+
